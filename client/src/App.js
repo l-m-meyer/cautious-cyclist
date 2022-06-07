c35d9b42f26e18c7d5afc6ab2bd15fc7e7e3e57a
Lisa Meyer <lisamermey@gmail.com>
2022-06-07 19:19:43 -0400

    remove Tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='display'>Cautious Cyclist</h1>
+      <h1>Cautious Cyclist</h1>
     </div>
   );
 }
